fb4512e8689e24d938aabef1d5a0925d08626627
Gyanendra Sahoo <gyanendrasahoo577@gmail.com>
2025-09-29 09:24:15 +0530

    Modify the Config file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
