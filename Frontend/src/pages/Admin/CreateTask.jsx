7dbd51556d9e1f249d63318d028a818f8cff636e
Gyanendra Sahoo <gyanendrasahoo577@gmail.com>
2025-09-30 10:46:38 +0530

    Path Correction

--- Frontend/src/pages/Admin/CreateTask.jsx
+++ Frontend/src/pages/Admin/CreateTask.jsx
@@ -7,12 +7,12 @@ import toast from "react-hot-toast";
 import { useLocation, useNavigate } from "react-router-dom";
 import moment from "moment";
 import { LuTrash2 } from "react-icons/lu";
-import SelectDropdown from "../../components/Input/SelectDropdown.jsx";
-import SelectUsers from "../../components/Input/SelectUsers.jsx";
-import TodoListInput from "../../components/Input/TodoListInput.jsx";
-import AddAttachmentsInput from "../../components/Input/AddAttachmentsInput.jsx";
-import Modal from "../../components/Modal.jsx";
-import DeleteAlert from "../../components/DeleteAlert.jsx";
+import SelectDropdown from "../../components/Input/SelectDropdown";
+import SelectUsers from "../../components/Input/SelectUsers";
+import TodoListInput from "../../components/Input/TodoListInput";
+import AddAttachmentsInput from "../../components/Input/AddAttachmentsInput";
+import Modal from "../../components/Modal";
+import DeleteAlert from "../../components/DeleteAlert";
 
 const CreateTask = () => {
   const location = useLocation();
